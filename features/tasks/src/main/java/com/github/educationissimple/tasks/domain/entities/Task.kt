0db79e563f10a5f8b901993f5e9bb21b2544fbcc
senya <alarol@mail.ru>
2024-10-09 21:23:55 +0300

    Added isCompleted argument for Task.

--- features/tasks/src/main/java/com/github/educationissimple/tasks/domain/entities/Task.kt
+++ features/tasks/src/main/java/com/github/educationissimple/tasks/domain/entities/Task.kt
@@ -1,4 +1,4 @@
 package com.github.educationissimple.tasks.domain.entities
 
 
-data class Task(val id: Int = 0, val text: String, val date: String? = null)
+data class Task(val id: Int = 0, val text: String, val isCompleted: Boolean = false, val date: String? = null)
