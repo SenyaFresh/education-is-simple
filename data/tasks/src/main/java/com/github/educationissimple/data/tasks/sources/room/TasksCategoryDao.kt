cc2f92ce4ee59ee96b4f71382a908220165da558
senya <alarol@mail.ru>
2024-10-15 19:58:28 +0300

    Now category with existing name does not adding to repo.

--- data/tasks/src/main/java/com/github/educationissimple/data/tasks/sources/room/TasksCategoryDao.kt
+++ data/tasks/src/main/java/com/github/educationissimple/data/tasks/sources/room/TasksCategoryDao.kt
@@ -2,6 +2,7 @@ package com.github.educationissimple.data.tasks.sources.room
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.github.educationissimple.data.tasks.entities.TaskCategoryDataEntity
 
@@ -11,10 +12,10 @@ interface TasksCategoryDao {
     @Query("SELECT * FROM task_categories")
     suspend fun getCategories(): List<TaskCategoryDataEntity>
 
-    @Insert(entity = TaskCategoryDataEntity::class)
+    @Insert(entity = TaskCategoryDataEntity::class, onConflict = OnConflictStrategy.ABORT)
     suspend fun createCategory(name: String)
 
     @Query("DELETE FROM task_categories WHERE id = :id")
     suspend fun deleteCategory(id: Long)
-    
+
 }
