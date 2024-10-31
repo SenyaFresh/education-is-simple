746d0ef2e6f10157e5b32dc418cad9307bc1b33e
senya <alarol@mail.ru>
2024-10-31 15:23:04 +0300

    Container loading fix.

--- core/presentation/src/main/java/com/github/educationissimple/presentation/ResultContainerComposable.kt
+++ core/presentation/src/main/java/com/github/educationissimple/presentation/ResultContainerComposable.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -73,7 +74,7 @@ fun ResultContainerComposable(
             }
 
             is ResultContainer.Loading -> {
-                var showLoading by mutableStateOf(false)
+                var showLoading by remember { mutableStateOf(false) }
                 LaunchedEffect(Unit) {
                     delay(50)
                     showLoading = true
